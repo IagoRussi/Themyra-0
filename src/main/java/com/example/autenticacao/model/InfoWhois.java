6dde0a229db3ce72c8f4cf6c808460a0853d8046
GustavoZP <gustavozarthperotto@gmail.com>
2025-08-12 08:44:59 -0300

    Atualização Whois

--- src/main/java/com/example/autenticacao/model/InfoWhois.java
+++ src/main/java/com/example/autenticacao/model/InfoWhois.java
@@ -1,5 +1,70 @@
 package com.example.autenticacao.model;
 
+import java.time.LocalDate;
+
 public class InfoWhois {
 
+    private String nomeDominio;
+    private LocalDate dataAtualizacao;
+    private LocalDate dataCriacao;
+    private LocalDate dataExpiracao;
+    private String nomeRegistrar;
+    private Boolean dominioPrivado;
+    private Boolean registrarPrivado;
+
+    public String getNomeDominio() {
+        return nomeDominio;
+    }
+
+    public void setNomeDominio(String nomeDominio) {
+        this.nomeDominio = nomeDominio;
+    }
+
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataExpiracao() {
+        return dataExpiracao;
+    }
+
+    public void setDataExpiracao(LocalDate dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public String getNomeRegistrar() {
+        return nomeRegistrar;
+    }
+
+    public void setNomeRegistrar(String nomeRegistrar) {
+        this.nomeRegistrar = nomeRegistrar;
+    }
+
+    public Boolean isPrivado() {
+        return dominioPrivado;
+    }
+
+    public void setDominioPrivado(Boolean dominioPrivado) {
+        this.dominioPrivado = dominioPrivado;
+    }
+
+    public Boolean isRegistrarPrivado() {
+        return registrarPrivado;
+    }
+
+    public void setRegistrarPrivado(Boolean registrarPrivado) {
+        this.registrarPrivado = registrarPrivado;
+    }
 }
--- src/main/java/com/example/autenticacao/service/AnaliseService.java
+++ src/main/java/com/example/autenticacao/service/AnaliseService.java
@@ -1,4 +1,24 @@
 package com.example.autenticacao.service;
 
+import com.example.autenticacao.model.Analise;
+import com.example.autenticacao.model.InfoJsoup;
+import com.example.autenticacao.model.InfoWhois;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnaliseService {
+
+    private final InfoWhoisService whoisService;
+
+    public AnaliseService(InfoWhoisService whoisService) {
+        this.whoisService = whoisService;
+    }
+
+    public Analise analise(String url) {
+        String rawWhois = whoisService.whoisRaw(url);
+        InfoWhois whois = whoisService.parseWhoisRaw(rawWhois);
+
+        return new Analise(whois);
+    }
+
 }
